2b9d7717801a0859890f5f79c4dc9b5afd09a687
Manuel Rebolledo <mcelenr@gmail.com>
2025-07-28 18:24:28 -0400

    Prueba E2E frontend finalizado

---
+++ cypress/e2e/pruebas_ordenadas.cy.js
@@ -0,0 +1,6 @@
+
+import './features/login/login.feature';
+import './features/registro/registro.feature';
+import './features/actualizar/actualizar.feature';
+import './features/eliminar/eliminar.feature';
+import './features/acceso/acceso_denegado.feature';
